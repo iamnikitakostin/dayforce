d368dc3e19769098dc34bbd98d6747b5cf8b74b1
Nikita Kostin <contact@nikitakostin.com>
2024-05-19 15:14:31 -0700

    set lazy loading for img & converted jpeg to webp

--- src/components/MyTeam/Member.js
+++ src/components/MyTeam/Member.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Member.css';
 
 function Member({ profile }) {
-  const profileImage = require(`../../assets/profiles/${profile.id}.jpeg`);
+  const profileImage = require(`../../assets/profiles/${profile.id}.webp`);
   return (
     <div className="member">
       <div className="member__img">
-        <img src={profileImage} alt="profile" />
+        <img src={profileImage} alt="profile" loading="lazy"/>
       </div>
       <div className="member__details">
         <p className="member__name p__regular">{profile.name}</p>
